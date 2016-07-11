fc8f133c1d68a00746a4c4ce8b5ed070bd5c8a3c
EMG114 <egutierrez411@gmail.com>
2016-07-11 18:13:49 -0400

    done

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -13,7 +13,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        topLeftLabel.text = "?"
+        middleLabel.text = "?"
+        bottomRightLabel.text = "?"
+        
+        
     }
 
     // TODO: IB actions and code to update UI
+    
+    @IBOutlet weak var topLeftLabel: UILabel!
+    
+    @IBOutlet weak var middleLabel: UILabel!
+    
+    @IBOutlet weak var bottomRightLabel: UILabel!
+    
+    
+    
+    @IBAction func fourOfClubs(sender: AnyObject) {
+        topLeftLabel.text = "♣️"
+        middleLabel.text = "4"
+        bottomRightLabel.text = "♣️"
+        
+    }
+    
+    @IBAction func threeOfSpades(sender: AnyObject) { topLeftLabel.text = "♠️"
+        middleLabel.text = "3"
+        bottomRightLabel.text = "♠️"
+    }
+    
+    @IBAction func eightOfDiamonds(sender: AnyObject) { topLeftLabel.text = "♦️"
+        middleLabel.text = "8"
+        bottomRightLabel.text = "♦️"
+    }
+    
+    
+    @IBAction func tenOfHearts(sender: AnyObject) { topLeftLabel.text = "♥️"
+        middleLabel.text = "10"
+        bottomRightLabel.text = "♥️"
+    }
+    
+    
 }
